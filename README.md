a87719a53942d68aae352de77290a8d8bf6a3c5c
Carl Wang <carl.w@wustl.edu>
2024-11-18 20:23:52 -0600

    found working solution using mjpg stream

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+To run the stream, run this command:
+```
+
+./mjpg_streamer -i "./input_uvc.so -d /dev/video0 -r 640x480 -f 30" -o "./output_http.so -p 8080 -w ./www"
+
+```
+
+Then visit 
+```
+http://carlpi:8080
+```
+
+Navigate to the stream section
+
+
+
+## below is deprecated
 # picamera-stream
 Flask server that hosts a real time stream from pi camera.
 
@@ -7,7 +24,7 @@ A flask server
 ## how to run
 1. Open terminal
 2. ```cd PATH\TO\FOLDER```
-3. Activate the virtual environment ```source /pienv/source/activate```
+3. Activate the virtual environment ```source /pienv/bin/activate```
 4. Run ```python3/python serve.py```
 5. Navigate to ```http://ip_to_server/camera```
 
--- serve.py
+++ serve.py
@@ -6,16 +6,19 @@ app = Flask(__name__)
 
 def generate_frames():
     # Initialize the OpenCV camera
-    camera = cv2.VideoCapture(0)  # 0 is usually the default camera index
+    camera = cv2.VideoCapture(0, cv2.CAP_V4L2)  # Use the V4L2 backend
+    # 0 is usually the default camera index
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # Set higher resolution
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
     camera.set(cv2.CAP_PROP_FPS, 24)  # Slightly reduce FPS for better performance
     camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))  # Use MJPEG for fast encoding
+    print("camera initialized")
 
-    time.sleep(2)  # Camera warm-up time
+    time.sleep(10)  # Camera warm-up time
     while True:
         success, image = camera.read()  # Capture frame-by-frame
         if not success:
+            print("camera read failed")
             break
         # Encode the frame as JPEG with optimized quality
         ret, buffer = cv2.imencode('.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 60])
@@ -27,7 +30,7 @@ def generate_frames():
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Flask main page"
 
 
 @app.route('/camera')
